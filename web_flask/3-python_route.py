f4728c61ea206c8e3201f4f5cab6c60e11c115ca
AJS9091 <ashtonjamess@live.com>
2022-03-20 06:56:54 -0700

    no more f-strings

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -9,20 +9,24 @@ app = Flask(__name__)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     no_ = text.replace('_', ' ')
-    return f'C {no_}'
+    return 'C %s' % no_
+
 
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text='is cool'):
         no_ = text.replace('_', ' ')
-            return f'Python {no_}'
+            return 'Python %s' % no_
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
