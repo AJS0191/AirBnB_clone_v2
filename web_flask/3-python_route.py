56271c41e930abccedb5dc7ea0d7853e6b50b742
AJS9091 <ashtonjamess@live.com>
2022-03-20 06:41:07 -0700

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,28 @@
+]#!/usr/bin/python3
+""" adding second route to flask application """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    no_ = text.replace('_', ' ')
+    return f'C {no_}'
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text='is cool'):
+        no_ = text.replace('_', ' ')
+            return f'Python {no_}'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
